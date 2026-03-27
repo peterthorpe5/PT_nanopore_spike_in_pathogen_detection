1784e986a4c28460b278601bdef14f9d29c06a17
peterthorpe5 <pthorpe001@dundee.ac.uk>
2026-03-27 13:24:44 +0000

    metabuli

--- shells/mk_Metabuli_db.sh
+++ shells/mk_Metabuli_db.sh
@@ -12,7 +12,7 @@ cd /home/pthorpe001/data/2026_plasmodium_kraken_sensitivity
 
 
 
-python /home/pthorpe001/data/2026_plasmodium_kraken_sensitivity/PT_nanopore_spike_in_pathogen_detection/scripts/prepare_kraken_library_for_metamaps.py \
+python /home/pthorpe001/data/2026_plasmodium_kraken_sensitivity/PT_nanopore_spike_in_pathogen_detection/scripts/prepare_kraken_libraries_for_metabuli.py \
   --input_fastas \
     /home/pthorpe001/data/project_back_up_2024/kraken_bact_virus_plasmo_fungal/library/library.fna \
     /home/pthorpe001/data/project_back_up_2024/kraken_bact_virus_plasmo_fungal/library/viral/library.fna \
